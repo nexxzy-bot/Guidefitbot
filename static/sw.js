8b1c7b5c4b8e651d5cbae534d53ffbfa7ce6e992
root <root@glamorous-copper.ptr.network>
2026-09-11 04:02:32 +0000

    полный слепок сервера

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'guidefit-v6';
+const CACHE = 'guidefit-v7';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
 self.addEventListener('fetch', e => {
